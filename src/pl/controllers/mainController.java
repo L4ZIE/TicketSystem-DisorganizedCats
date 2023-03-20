8c139124786251c282e5e9220ca2e5f0ede0a6e2
Bálint <47783658+Roharx@users.noreply.github.com>
2023-03-20 15:47:46 +0100

    Finished User Controls and CSS

--- src/pl/controllers/mainController.java
+++ src/pl/controllers/mainController.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,20 +21,57 @@ public class mainController implements Initializable {
 
 
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         displayUserControls(anpController);
+        displayEventsTableView(anpContent);
+        anpMain.setStyle("-fx-background-color: #474747");
     }
 
     private void displayUserControls(AnchorPane container) {
         //TODO
-        container.getStyleClass().add("controls-container");
+        container.getStyleClass().add("container");
 
+        Button manageUsers = new Button();
+        Button manageEvents = new Button();
+        Button manageSpecTickets = new Button();
+        Label userControls = new Label();
 
+        VBox buttonContainer = new VBox(5);
+
+        manageUsers.getStyleClass().add("app-buttons");
+        manageEvents.getStyleClass().add("app-buttons");
+        manageSpecTickets.getStyleClass().add("app-buttons");
+
+        manageUsers.setText("Manage Users");
+        manageEvents.setText("Manage Events");
+        manageSpecTickets.setText("Manage Special Tickets");
+        userControls.setText("User Controls");
+
+        manageUsers.setMinWidth(container.getMinWidth() - 20);
+        manageUsers.setMinHeight((container.getMinHeight() / 3) - 30);
+
+        manageEvents.setMinWidth(manageUsers.getMinWidth());
+        manageEvents.setMinHeight(manageUsers.getMinHeight());
+
+        manageSpecTickets.setMinWidth(manageEvents.getMinWidth());
+        manageSpecTickets.setMinHeight(manageEvents.getMinHeight());
+
+        buttonContainer.getChildren().addAll(manageUsers, manageEvents, manageSpecTickets);
+        buttonContainer.setLayoutX(container.getLayoutX() + 3);
+        buttonContainer.setLayoutY(container.getLayoutY() + 50);
+        buttonContainer.setMinHeight(container.getMinHeight() - 20);
+
+        userControls.setLayoutX((container.getMinWidth() / 2) - 50);
+        userControls.setLayoutY(container.getLayoutY() + 10);
+
+        container.getChildren().addAll(userControls, buttonContainer);
     }
 
     private void displayEventsTableView(AnchorPane container) {
         //TODO
+        container.getStyleClass().add("container");
     }
 
     private void HomeScreen(AnchorPane container) {
