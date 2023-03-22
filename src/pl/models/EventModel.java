99f73f932debdff08ac223fa23b8f465b4ff6f34
Bálint <47783658+Roharx@users.noreply.github.com>
2023-03-22 12:25:06 +0100

    Created base for models

---
+++ src/pl/models/EventModel.java
@@ -0,0 +1,36 @@
+package pl.models;
+
+import be.Event;
+import bll.EventManager;
+import javafx.collections.ObservableList;
+
+public class EventModel {
+
+    EventManager eventManager;
+    public EventModel(){
+        eventManager = new EventManager();
+    }
+
+    public ObservableList<Event> getAllEvents(){
+        //waiting for backend
+        return null;
+    }
+    public Event getEventByID(int id){
+        //waiting for backend
+        return null;
+    }
+    public ObservableList<Event> getEventByAccount(){
+        //sprint 2
+        return null;
+    }
+    public ObservableList<Event> getEventsByDate(String date){
+        //waiting for backend
+        return null;
+    }
+    public ObservableList<Event> getEventsByName(String name){
+        //waiting for backend
+        return null;
+    }
+
+
+}
