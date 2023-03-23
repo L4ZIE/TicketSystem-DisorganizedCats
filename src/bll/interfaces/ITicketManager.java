fabea19684356b8d0ab424a0f84f6b0992906b54
L4ZIE <112688751+L4ZIE@users.noreply.github.com>
2023-03-23 20:22:34 +0100

    Revert "I implemented the methods that were missing in TicketManager and Spec…"

--- src/bll/interfaces/ITicketManager.java
+++ src/bll/interfaces/ITicketManager.java
@@ -1,6 +1,5 @@
 package bll.interfaces;
 
-import be.Event;
 import be.Ticket;
 
 import java.sql.SQLException;
@@ -12,14 +11,5 @@ public interface ITicketManager {
     void deleteTicket(int id);
     List<Ticket> searchTicketsByCustomerName(String query);
     Ticket getTicketByID(int id);
-    List<Ticket>searchTicketByEvent(String query);
-    List<Ticket> getTicketsByCustomer(String name);
-
-    List<Ticket> getTicketsByEmail(String email);
-
-    List<Ticket> getTicketsByPrice(int price);
-
-    List<Ticket>  getTicketsByUsed(Boolean used);
-
-    List<Ticket> getTicketsByEventID(Event event);
+    void searchTicketByEvent(String query);
 }
